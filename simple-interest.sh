318f172d029c81906c7452c1b51e2fc0067c3dd7
CodeMystique <141498914+CodeInfiltrator@users.noreply.github.com>
2024-01-13 11:39:30 +0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,16 +1,21 @@
-#!/bin/bash
+   #!/bin/bash
    # This script calculates simple interest given principal,
    # annual rate of interest and time period in years.
+   
    # Do not use this in production. Sample purpose only.
+   
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # CodeInfiltrator
+   
    # Input:
    # p, principal amount
    # t, time period in years
    # r, annual rate of interest
+   
    # Output:
    # simple interest = p*t*r
+   
    echo "Enter the principal:"
    read p
    echo "Enter rate of interest per year:"
